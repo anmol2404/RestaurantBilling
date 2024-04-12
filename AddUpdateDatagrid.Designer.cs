4c5f8d26746402e2fb2c70cdc1c852036ae75c7d
anmol sharma <sharma2404anmol@outlook.com>
2024-04-12 19:30:14 +0530

    4

--- AddUpdateDatagrid.Designer.cs
+++ AddUpdateDatagrid.Designer.cs
@@ -10,7 +10,7 @@
 
 #pragma warning disable 1591
 
-namespace WindowsFormsApp1 {
+namespace Tulip {
     
     
     /// <summary>
@@ -682,7 +682,7 @@ namespace WindowsFormsApp1 {
         }
     }
 }
-namespace WindowsFormsApp1.AddUpdateDatagridTableAdapters {
+namespace Tulip.AddUpdateDatagridTableAdapters {
     
     
     /// <summary>
